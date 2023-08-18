9b6042528535b22f1ba9b351036976d7a85b9f77
Dylbill-Iroh <138424684+Dylbill-Iroh@users.noreply.github.com>
2023-08-18 00:42:14 -0400

    Add files via upload

---
+++ DbSkseFunctions/Source/STLThunk.h
@@ -0,0 +1,35 @@
+namespace stl
+{
+	using namespace SKSE::stl;
+
+	template <class T>
+	void write_thunk_call(std::uintptr_t a_src)
+	{
+		SKSE::AllocTrampoline(14);
+		auto& trampoline = SKSE::GetTrampoline();
+		T::func = trampoline.write_call<5>(a_src, T::thunk);
+	}
+
+	template <class F, std::size_t idx, class T>
+	void write_vfunc()
+	{
+		REL::Relocation<std::uintptr_t> vtbl{ F::VTABLE[0] };
+		T::func = vtbl.write_vfunc(idx, T::thunk);
+	}
+
+	template <std::size_t idx, class T>
+	void write_vfunc(REL::VariantID id)
+	{
+		REL::Relocation<std::uintptr_t> vtbl{ id };
+		T::func = vtbl.write_vfunc(idx, T::thunk);
+	}
+
+	template <class T>
+	void write_thunk_jump(std::uintptr_t a_src)
+	{
+		SKSE::AllocTrampoline(14);
+
+		auto& trampoline = SKSE::GetTrampoline();
+		T::func = trampoline.write_branch<5>(a_src, T::thunk);
+	}
+}
